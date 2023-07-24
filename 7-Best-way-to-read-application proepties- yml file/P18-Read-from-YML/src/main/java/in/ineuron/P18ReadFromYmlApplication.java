ff5c99cee66f00ba841e483d38e951676f43eaa5
Safvan <safvanpmcz@gmail.com>
2023-07-25 04:48:10 +0530

    showing best way to read conatsnt from  YML or preopties file

---
+++ 7-Best-way-to-read-application proepties- yml file/P18-Read-from-YML/src/main/java/in/ineuron/P18ReadFromYmlApplication.java
@@ -0,0 +1,13 @@
+package in.ineuron;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class P18ReadFromYmlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(P18ReadFromYmlApplication.class, args);
+	}
+
+}
